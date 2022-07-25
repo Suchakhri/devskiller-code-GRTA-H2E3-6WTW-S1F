91ba6cad01033910ba2d33814441b521cea1d1c0
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-26 01:13:49 +0700

    1st

--- database/db.js
+++ database/db.js
@@ -1,11 +1,13 @@
 import mysql from "serverless-mysql";
 const db = mysql({
   config: {
-    host: process.env.db_host,
-    port: process.env.db_port,
-    database: process.env.db_database,
-    user: process.env.db_user,
-    password: process.env.db_password,
+    host:
+      process.env.db_host ||
+      "aws-vercel.cbyrumhoz69l.us-west-1.rds.amazonaws.com",
+    port: process.env.db_port || "3306",
+    database: process.env.db_database || "vercel_aws",
+    user: process.env.db_user || "root",
+    password: process.env.db_password || "Manutsa_003028",
   },
 });
 export default async function excuteQuery({ query, values }) {
